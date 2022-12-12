90fcb3e7c66f2a0706d59e42c346fc0a8fe471d8
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-12-12 12:33:07 +0300

    feat: adding exception handler for 404 code

---
+++ src/main/java/app/prog/service/ApiExceptionHandler.java
@@ -0,0 +1,17 @@
+package app.prog.service;
+
+import app.prog.model.exception.ApiException;
+import app.prog.model.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+    @ExceptionHandler(value = NotFoundException.class)
+    public ResponseEntity<ApiException> handleApiRequestException(NotFoundException e){
+        ApiException apiException = new ApiException(e.getMessage(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(apiException, HttpStatus.NOT_FOUND);
+    }
+}
